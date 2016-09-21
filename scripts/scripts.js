2e164856a02c3512b0ae589414a17d4443f575ed
haim1993 <haim1993@yahoo.com>
2016-09-21 23:44:50 +0300

    added new language support

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+
+  var images = [], x = -1;
+  images[0] = "stylesheets/Images/profile.jpg";
+  images[1] = "stylesheets/Images/profile_switch.jpg";
+  
+  function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+  }
+
+  function startTimer() {
+    setInterval(displayNextImage, 1000);
+  }
+  
+ // function reloadFunction() {
+  // location.reload();
+//  }
