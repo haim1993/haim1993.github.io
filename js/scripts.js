2f9f4fb27e299192d249416ecbbb048b7f570967
Shlez <shlez1993.hs@gmail.com>
2017-08-22 18:51:45 +0300

    fixed resume button

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
     // Change language text + resume
     $("#nav-home").css('color', '#A9A9A9');
 
-    $(".select-language").click(function() {
-        if ($(".language").text() == 'EN') {
-            $(".flag").attr("src", "img/il.svg");
-            $(".language").text("HE");
-            $("#title").text("Hi. I'm Haim.");
-            $("#skills").text("Software Developer, Freelance");
-            $("#details").text("Click here and checkout my resume!");
-            $(".button").attr("onClick", "window.open('docs/resume-en.pdf');")
-        } else {
-            $(".flag").attr("src", "img/us.svg");
-            $(".language").text("EN");
-            $("#title").text(".שלום. שמי חיים");
-            $("#skills").text("מפתח תוכנה, פרילנס");
-            $("#details").text("!לחצו כאן לקורות חיים");
-            $(".button").attr("onClick", "window.open('docs/resume-he.pdf');")
-        }
-    });
+    // $(".select-language").click(function() {
+    //     if ($(".language").text() == 'EN') {
+    //         $(".flag").attr("src", "img/il.svg");
+    //         $(".language").text("HE");
+    //         $("#title").text("Hi. I'm Haim.");
+    //         $("#skills").text("Software Developer, Freelance");
+    //         $("#details").text("Click here and checkout my resume!");
+    //         $(".button").attr("onClick", "window.open('docs/resume-en.pdf');")
+    //     } else {
+    //         $(".flag").attr("src", "img/us.svg");
+    //         $(".language").text("EN");
+    //         $("#title").text(".שלום. שמי חיים");
+    //         $("#skills").text("מפתח תוכנה, פרילנס");
+    //         $("#details").text("!לחצו כאן לקורות חיים");
+    //         $(".button").attr("onClick", "window.open('docs/resume-he.pdf');")
+    //     }
+    // });
 
     if ($(window).width() > 990) {
         $("#nav-projects").click(function() {
@@ -33,9 +33,9 @@ $(document).ready(function() {
                     'right': '36%',
                     'margin-left': '10px'
                 });
-                $("#project-frame").fadeIn(900);
+                $("#project-container").fadeIn(900);
                 $("#project-info").fadeIn(1200);
-                $("#title, #skills, #details, #resume-btn").fadeOut("fast");
+                $("#profile-info").fadeOut("fast");
 
             }
         });
@@ -54,8 +54,8 @@ $(document).ready(function() {
                     'margin-left': '0px'
                 });
                 $("#project-info").hide();
-                $("#project-frame").fadeOut("fast");
-                $("#title, #skills, #details, #resume-btn").fadeIn("fast");
+                $("#project-container").fadeOut("fast");
+                $("#profile-info").fadeIn("fast");
             }
         });
     }
