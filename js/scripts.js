bd303ddee14507ad9b11e51c4802b4d464cd04e7
Shlez <shlez1993.hs@gmail.com>
2017-08-22 17:53:02 +0300

    Added project navigation bar but not responsive yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,62 @@
+$(document).ready(function() {
+    // Change language text + resume
+    $("#nav-home").css('color', '#A9A9A9');
 
-  var images = [], x = -1;
-  images[0] = "stylesheets/Images/profile.jpg";
-  images[1] = "stylesheets/Images/profile.jpg";
-  
-  function displayNextImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
-    document.getElementById("img").src = images[x];
-  }
+    $(".select-language").click(function() {
+        if ($(".language").text() == 'EN') {
+            $(".flag").attr("src", "img/il.svg");
+            $(".language").text("HE");
+            $("#title").text("Hi. I'm Haim.");
+            $("#skills").text("Software Developer, Freelance");
+            $("#details").text("Click here and checkout my resume!");
+            $(".button").attr("onClick", "window.open('docs/resume-en.pdf');")
+        } else {
+            $(".flag").attr("src", "img/us.svg");
+            $(".language").text("EN");
+            $("#title").text(".שלום. שמי חיים");
+            $("#skills").text("מפתח תוכנה, פרילנס");
+            $("#details").text("!לחצו כאן לקורות חיים");
+            $(".button").attr("onClick", "window.open('docs/resume-he.pdf');")
+        }
+    });
 
-  function startTimer() {
-    setInterval(displayNextImage, 10000);
-  }
-  
- // function reloadFunction() {
-  // location.reload();
-//  }
+    if ($(window).width() > 990) {
+        $("#nav-projects").click(function() {
+            $("#nav-projects").css('color', '#A9A9A9');
+            $("#nav-home").css('color', '#0275d8');
+            $right = document.getElementById('profile-container').style.right;
+            if ($right != '36%') {
+                $("#profile-container").css({
+                    'right': '',
+                    'left': ''
+                }).animate({
+                    'right': '36%',
+                    'margin-left': '10px'
+                });
+                $("#project-frame").fadeIn(900);
+                $("#project-info").fadeIn(1200);
+                $("#title, #skills, #details, #resume-btn").fadeOut("fast");
+
+            }
+        });
+
+        $("#nav-home").click(function() {
+            $("#nav-home").css('color', '#A9A9A9');
+            $("#nav-projects").css('color', '#0275d8');
+            $right = document.getElementById('profile-container').style.right;
+            $left = document.getElementById('profile-container').style.left;
+            if ($left != '0%') {
+                $("#profile-container").css({
+                    'right': $right,
+                    'left': $left
+                }).animate({
+                    'right': '0%',
+                    'margin-left': '0px'
+                });
+                $("#project-info").hide();
+                $("#project-frame").fadeOut("fast");
+                $("#title, #skills, #details, #resume-btn").fadeIn("fast");
+            }
+        });
+    }
+});
